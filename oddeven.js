1e68b3d2429b54cc68b67028764c2d57681a779c
SVK5227D <svinothkumar0301@gmail.com>
2023-03-15 18:05:25 +0530

    checkcomplete

--- oddeven.js
+++ oddeven.js
@@ -1,1 +1,8 @@
-let value
+let value = prompt("Enter your value to check odd or even");
+
+if(value%2 == 0){
+    console.log( value + " is even number");
+}
+else{
+    console.log( value + " is odd number");
+}
